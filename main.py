8a0fce2f81e2220060483e1d185b6036bfcc7300
sakuyacatcat <shingo.takagi0116@gmail.com>
2024-09-04 00:02:23 +0900

    prompts

--- main.py
+++ main.py
@@ -1,29 +1,6 @@
-from langchain.prompts import (
-    ChatPromptTemplate,
-    HumanMessagePromptTemplate,
-    SystemMessagePromptTemplate,
-)
-from langchain_groq import ChatGroq
+def main():
+    print("Hello")
 
-llm = ChatGroq(
-    model="mixtral-8x7b-32768",
-    temperature=0,
-    max_tokens=None,
-    timeout=None,
-    max_retries=2,
-)
 
-chat_prompt = ChatPromptTemplate.from_messages(
-    [
-        SystemMessagePromptTemplate.from_template(
-            "あなたは{country}料理のプロフェッショナルです"
-        ),
-        HumanMessagePromptTemplate.from_template(
-            "以下の料理のレシピを考えてください。\n\n料理名: {dish}"
-        ),
-    ]
-)
-messages = chat_prompt.format_prompt(country="イギリス", dish="肉じゃが")
-
-ai_msg = llm.invoke(messages)
-print(ai_msg.content)
+if __name__ == "__main__":
+    main()
